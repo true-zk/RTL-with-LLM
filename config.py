9e8faa6e6e6efd68e93383243955176e4bbf11b4
Ken Zhong <zhongken@sjtu.edu.cn>
2025-04-15 23:12:08 +0800

    ...

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+root_ = os.path.dirname(os.path.abspath(__file__))
+
+# llm api
+BASE_URL = "http://111.186.56.172:3000/v1"
+API_KEY = "sk-e9ZHXSwNdl1gz43v135aCa39A8A943FeBeA059703a6dAfE5"
+MODEL_NAME = "deepseek-chat"
+
+# embedding model
+EMBEDDING_MODEL = os.path.join(root_, "bge-large-en-v1.5")
+
+# data path
+RAW_DATA_ROOT_DIR = os.path.join(root_, "data")
+RAG_ROOT_DIR = os.path.join(root_, "rag_data")
---
+++ llm.py
@@ -0,0 +1,16 @@
+from llama_index.llms.openai_like import OpenAILike
+
+from config import (
+    MODEL_NAME,
+    BASE_URL,
+    API_KEY,
+)
+
+
+llm = OpenAILike(
+    model=MODEL_NAME,
+    api_base=BASE_URL,
+    api_key=API_KEY,
+    temperature=0.1,
+    is_chat_model=True,
+)
